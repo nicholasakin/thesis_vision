51127e55c63b548028efc923925e602d23ec2c40
Nicholas Akin <49415242+nikakcin17@users.noreply.github.com>
2022-12-19 19:31:35 -0500

    camera in tkinter frame

--- src/camera.py
+++ src/camera.py
@@ -1,52 +1,92 @@
 """
+Camera application
 
 """
 
-#packages
+"""
+Packages
+"""
+
+#pip install opencv-python
+import tkinter as tk
 import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-import time #for use of sleep
+from PIL import Image, ImageTk
+import time 
+
+#dlib face recognition : $pip install dlib face_recognition
+import face_recognition
 
 
 
-def openCamera():
-    #camera channel
-    if (cv2.VideoCapture(0).isOpened()):
-        camera = cv2.VideoCapture(0)
+"""
+global vars
+"""
 
-    #video file
-    video_file = ""
 
-    while (True):
-        ret, frame = camera.read()
 
-        print('in loop')
 
-        # if frame is read correctly ret is True
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
+class camera:
+    def __init__(self, window, window_title, video_source=0):
+        self.window = window
+        self.window.title(window_title)
+        self.video_source = video_source
 
-        #display video frame
-        cv2.imshow('frame', frame)
+        # open video source (by default this will try to open the computer webcam)
+        self.vid = cv2.VideoCapture(self.video_source)
+
+        # Create a canvas that can fit the above video source size
+        self.canvas = tk.Canvas(window, width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH), height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.canvas.pack()
         
-        # 'q' to quit loop
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        
-        #TODO : enable 'X' window functino to quit
 
-        time.sleep(.5) #for video processing time
+        # Button that lets the user take a snapshot
+        self.btn_snapshot=tk.Button(window, text="Snapshot", width=50, command=self.snapshot)
+        self.btn_snapshot.pack(anchor=tk.CENTER, expand=True)
 
-
-    #releasing camera obj
-    camera.release()
-    #destroy windows
-    cv2.destroyAllWindows()
+        # After it is called once, the update method will be automatically called every delay milliseconds
+        self.delay = 15
+        self.update()
 
 
+        #runs application
+        self.window.mainloop()
+
+    def snapshot(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.read()
+
+        if ret:
+            cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+
+    def update(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.read()
 
 
-if __name__ == '__main__':
-    openCamera()
+        if ret:
+            # Convert frame (BGR) to image (RGB)
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            ## Use face_recognition for face detection in image
+            face_locations = face_recognition.face_locations(image)
+
+            ## Draw rectangle around each face
+            #color in BGR, coordinates for topleft and bottomright of rectangle
+            for top, right, bottom, left in face_locations:
+                cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2) 
+
+            # Convert the image back to a PhotoImage object
+            self.photo = Image.fromarray(image)
+            self.photo = ImageTk.PhotoImage(image=self.photo)
+
+            # Display the image on the canvas
+            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+
+        #After processing and delay, update image
+        self.window.after(self.delay, self.update)
+
+
+
+# Create a window and pass it to the Application object
+camera(tk.Tk(), "Scorpion")
+
